6062b6b9a08f03dc7598918400502bcf67989525
JulienMedina <julien.medina16@gmail.com>
2024-12-06 16:33:22 +0100

    test transfer data render

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,13 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
+
+  transfer: {
+    remote: {
+      enabled: true, // Assurez-vous que cette valeur est à true
+    },
+  }, // Désactive les WebSockets
 });
